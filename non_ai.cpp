ba0d3376c1ca1060e7acca46dbb37dcfcc49609d
Harsh Doshi <156217741+Har1008@users.noreply.github.com>
2024-05-28 22:10:01 +0530

    Create non_ai.cpp

---
+++ non_ai.cpp
@@ -0,0 +1,89 @@
+#include <iostream>
+
+#define SIZE 3
+#define PLAYER1 'X'
+#define PLAYER2 'O'
+#define EMPTY ' '
+
+class GameState {
+public:
+    char board[SIZE][SIZE];
+
+    GameState() {
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                board[i][j] = EMPTY;
+            }
+        }
+    }
+
+    void displayBoard() {
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                std::cout << " " << board[i][j] << " ";
+                if (j < SIZE - 1) std::cout << "|";
+            }
+            std::cout << "\n";
+            if (i < SIZE - 1) std::cout << "---|---|---\n";
+        }
+    }
+
+    bool isBoardFull() {
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                if (board[i][j] == EMPTY) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    bool checkWin(char player) {
+        for (int i = 0; i < SIZE; i++) {
+            if (board[i][0] == player && board[i][1] == player && board[i][2] == player)
+                return true;
+            if (board[0][i] == player && board[1][i] == player && board[2][i] == player)
+                return true;
+        }
+        if (board[0][0] == player && board[1][1] == player && board[2][2] == player)
+            return true;
+        if (board[0][2] == player && board[1][1] == player && board[2][0] == player)
+            return true;
+        return false;
+    }
+
+    void playerMove(char player) {
+        int row, col;
+        do {
+            std::cout << "Player " << player << ", enter your move (row and column): ";
+            std::cin >> row >> col;
+        } while (row < 1 || row > SIZE || col < 1 || col > SIZE || board[row - 1][col - 1] != EMPTY);
+        board[row - 1][col - 1] = player;
+    }
+
+    void gameLoop() {
+        char currentPlayer = PLAYER1;
+        while (true) {
+            displayBoard();
+            playerMove(currentPlayer);
+            if (checkWin(currentPlayer)) {
+                displayBoard();
+                std::cout << "Player " << currentPlayer << " wins!\n";
+                break;
+            }
+            if (isBoardFull()) {
+                displayBoard();
+                std::cout << "The game is a draw!\n";
+                break;
+            }
+            currentPlayer = (currentPlayer == PLAYER1) ? PLAYER2 : PLAYER1;
+        }
+    }
+};
+
+int main() {
+    GameState state;
+    state.gameLoop();
+    return 0;
+}
